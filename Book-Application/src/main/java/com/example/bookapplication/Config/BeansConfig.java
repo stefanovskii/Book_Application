9ba428ecdd36de3f98b021d4a5988d51ed429414
ivan stefanovski <istefanovski46@gmail.com>
2025-01-08 14:27:33 +0100

    Changed the structure with new modules, created the frontend application in Angular and implemented the services with OpenAPI

--- Book-Application/src/main/java/com/example/bookapplication/Config/BeansConfig.java
+++ Book-Application/src/main/java/com/example/bookapplication/Config/BeansConfig.java
@@ -1,8 +1,10 @@
 package com.example.bookapplication.Config;
 
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -10,6 +12,11 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class BeansConfig {
